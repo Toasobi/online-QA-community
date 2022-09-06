8e53331f3265ce0a48d421f58b09fe8c7ebd0ba7
Toasobi <2683661364@qq.com>
2022-09-06 21:51:54 +0800

    Toasobi的提交

--- respository/answer.go
+++ respository/answer.go
@@ -8,11 +8,11 @@ import (
 
 type Answer struct {
 	gorm.Model
-	QuestionIdentity string `gorm:"column:question_identity;type:varchar(36);"json:"question_identity";` //问题表的唯一标识
-	Identity         string `gorm:"column:identity;type:varchar(36);"json:"identity"`                    //回答表的唯一标识
-	Content          string `gorm:"column:content;type:text;"json:"content"`                             //回答正文
-	Name             string `gorm:"column:name;type:varchar(100);"json:"name"`                           //回答提问的用户名
-	Like             int    `gorm:"column:like;type:int(11);"json:"like"`                                //点赞数
+	QuestionIdentity string `gorm:"column:question_identity;type:varchar(36);"json:"question_identity"` //问题表的唯一标识
+	Identity         string `gorm:"column:identity;type:varchar(36);"json:"identity"`                   //回答表的唯一标识
+	Content          string `gorm:"column:content;type:text;"json:"content"`                            //回答正文
+	Name             string `gorm:"column:name;type:varchar(100);"json:"name"`                          //回答提问的用户名
+	Like             int    `gorm:"column:like;type:int(11);"json:"like"`                               //点赞数
 }
 
 func (table *Answer) TableName() string {
--- respository/comment.go
+++ respository/comment.go
@@ -8,12 +8,12 @@ import (
 
 type Comment struct {
 	gorm.Model
-	QuestionIdentity string `gorm:"column:question_identity;type:varchar(36);"json:"question_identity";` //问题表的唯一标识
-	AnswerIdentity   string `gorm:"column:answer_identity;type:varchar(36);"json:"answer_identity";`     //文章唯一标识
-	Identity         string `gorm:"column:identity;type:varchar(36);"json:"identity"`                    //评论表的唯一标识
-	Content          string `gorm:"column:content;type:text;"json:"content"`                             //评论正文
-	Name             string `gorm:"column:name;type:varchar(100);"json:"name"`                           //回答文章的用户名
-	Like             int    `gorm:"column:like;type:int(11);"json:"like"`                                //点赞数
+	QuestionIdentity string `gorm:"column:question_identity;type:varchar(36);"json:"question_identity"` //问题表的唯一标识
+	AnswerIdentity   string `gorm:"column:answer_identity;type:varchar(36);"json:"answer_identity"`     //文章唯一标识
+	Identity         string `gorm:"column:identity;type:varchar(36);"json:"identity"`                   //评论表的唯一标识
+	Content          string `gorm:"column:content;type:text;"json:"content"`                            //评论正文
+	Name             string `gorm:"column:name;type:varchar(100);"json:"name"`                          //回答文章的用户名
+	Like             int    `gorm:"column:like;type:int(11);"json:"like"`                               //点赞数
 }
 
 func (table *Comment) TableName() string {
